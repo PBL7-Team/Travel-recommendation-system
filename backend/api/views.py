6248e4ca99d159c5cc00477a327909474223379b
Bach IT <87653572+Bach2k2@users.noreply.github.com>
2024-04-03 20:51:33 +0700

    create first api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import JsonResponse
+
+def api_data(request):
+    # Dummy data for demonstration
+    data = {
+        'message': 'Hello, this is my first API using Django!',
+        'status': 'success'
+    }
+    return JsonResponse(data)
